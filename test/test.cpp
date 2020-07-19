7ab9da8f80de56cb345e819148027994a6df24ab
RandomUserIK <xkovaci1@stuba.sk>
2020-07-19 18:00:36 +0200

    add travis config

---
+++ test/test.cpp
@@ -0,0 +1,5 @@
+#include "test.hpp"
+
+Test::Test()
+{
+}
---
+++ test/test.hpp
@@ -0,0 +1,12 @@
+#ifndef TEST_HPP
+#define TEST_HPP
+
+#include "test_global.h"
+
+class TEST_EXPORT Test
+{
+public:
+    Test();
+};
+
+#endif // TEST_HPP
